64f3d8495779c9f79c0eeac6777905d2e086dc16
Wonder <wonderdxk@gmail.com>
2020-06-26 22:56:05 -0400

    1.0.1
    Add elements to description plugin and update portfolio

--- Syntax-Shortcode.php
+++ Syntax-Shortcode.php
@@ -3,9 +3,12 @@
 Plugin Name: Syntax ShortCode
 Plugin URI: http://wordpress.org/
 Description: Shortcodes for WordPress
-Author: MetalSnytax 
-Version: 0.2
-Author URI: https://metalsyntax.blogspot.com
+Author: MetalSyntax 
+Version: 1.0.1
+Author URI: https://metalsyntax.now.sh/
+* License:     GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: shortcodes-elementor-plugin
 */
 
 /*
